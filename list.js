524ecbfecfe18752590f3f6e4acb8a8dc27f324c
Llithy <36874669+Llithy@users.noreply.github.com>
2020-03-26 12:51:25 +0100

    Update list.js
    Added timeout so that Chrome can show the HTML page with the commands before running the Javascript. Else you'll get the popup prompt from Javascript before the page loads which means you can't see the instructions / commands.

--- list.js
+++ list.js
@@ -1,19 +1,20 @@
 var todos = [];
+window.setTimeout(function() {
+    var input = prompt("What would you like to do?");
 
-var input = prompt("What would you like to do?");
-
-while(input !== "quit") {
-    //handle input
-    if(input === "list") {
-        console.log(todos);
-    } else if (input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todos array
-        todos.push(newTodo);
+    while(input !== "quit") {
+        //handle input
+        if(input === "list") {
+            console.log(todos);
+        } else if (input === "new") {
+            //ask for new todo
+            var newTodo = prompt("Enter new todo");
+            //add to todos array
+            todos.push(newTodo);
+        }
+        //ask again for new input
+        input = prompt("What would you like to do?");
+    
     }
-    //ask again for new input
-    input = prompt("What would you like to do?");
-
-}
-console.log("Ok, you quit the app")
+    console.log("Ok, you quit the app")
+  }, 500);
